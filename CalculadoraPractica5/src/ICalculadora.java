a99099c90b5655164987dff75b808a9b89453720
Daniel Sastre Hernandez <91556389+DSastreHernandez@users.noreply.github.com>
2022-05-15 21:56:15 +0200

    Añadido ICalculadora y Calculadora
    Implementada la interfaz ICalculadora y la clase Calculadora

---
+++ CalculadoraPractica5/src/ICalculadora.java
@@ -0,0 +1,11 @@
+public interface ICalculadora {
+    // Esta es la interfaz
+
+    public int sumar(int a, int b);
+
+    public int restar(int a, int b);
+
+    public int multiplicar(int a, int b);
+
+    public int dividir(int a, int b);
+}
