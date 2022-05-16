8ef419feb01dec5cc1c4df3c14386b0d1057b447
Daniel Sastre Hernandez <dsastre@cifpfbmoll.eu>
2022-05-16 14:38:09 +0200

    Proyecto acabado

---
+++ Calculadora/src/main/java/org/example/ICalculadora.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public interface ICalculadora {
+    // Esta es la interfaz
+
+    public float sumar(float a, float b);
+
+    public float restar(float a, float b);
+
+    public float multiplicar(float a, float b);
+
+    public float dividir(float a, float b);
+}
